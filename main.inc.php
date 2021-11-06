3577120ba0cfe200dd5008d656f276daace4cd4b
Eike Rathke <erack@erack.de>
2021-11-06 16:57:52 +0100

    Piwigo 12 needs mandatory Has Settings
    See https://github.com/Piwigo/Piwigo/wiki/Technical-changes-in-Piwigo-12

--- main.inc.php
+++ main.inc.php
@@ -5,6 +5,7 @@ Description: Uses Akismet online service to check comments agains spam
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=192
 Author: rvelices
 Author URI: http://www.modusoptimus.com
+Has Settings: true
 */
 
 define('AKIS_DIR' , basename(dirname(__FILE__)));
