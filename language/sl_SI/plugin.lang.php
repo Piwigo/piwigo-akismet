40d741f07a30ee97018752aa4a7b0750fded7c5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-16 19:25:13 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['comments'] = 'komentarjev';
+$lang['moderate'] = 'zmerno';
+$lang['out of'] = 'od';
+$lang['reject'] = 'zavrniti';
+$lang['reset counters'] = 'ponastavi števce';
+$lang['spam comments'] = 'vsiljenih komentarjev';
+$lang['akismet site'] = 'akismet strani';
+$lang['You need to define the Akismet api key'] = 'Definirati morate Akismet API ključ';
+$lang['Your Akismet API key is not valid.'] = 'Vaš API Akismet ključ ni veljaven.';
+$lang['This plugin verifies if your comments are spam using the <a href="http://www.akismet.com" target="_blank">akismet online service</a>.'] = 'Ta vtičnik preveri, ali so vaši komentarji vsiljeni z <a href="http://www.akismet.com" target="_blank">spletno storitvijo Akismet</a>.';
+$lang['Action when spam is detected:'] = 'Ukrep, ko je zaznana vsiljenost:';
+$lang['Akismet API Key:'] = 'Akismet API ključ:';
+$lang['Akismet configuration'] = 'Akismet konfiguracija';
+$lang['Akismet has caught'] = 'Akismet je ujel';
+$lang['More statistics on'] = 'Več statistike na';
+$lang['Signup for a personal usage key here:'] = 'Prijavite se za ključ za osebno uporabo tukaj:';
+$lang['Test your configuration'] = 'Testirajte svojo konfiguracijo';
