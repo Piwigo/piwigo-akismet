db554b1c6d04ab1685e0396dcfe74eff6261927b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-28 07:24:28 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You need to define the Akismet api key'] = 'Þú verður að skilgreina Akismet api lykilinn';
+$lang['Your Akismet API key is not valid.'] = 'Þinn Akismet API lykill er ekki gildur';
+$lang['akismet site'] = 'akismet vefsíða';
+$lang['comments'] = 'umsagnir';
+$lang['moderate'] = 'fundarstýra';
+$lang['out of'] = 'af';
+$lang['reject'] = 'hafna';
+$lang['reset counters'] = 'endursetja teljara';
+$lang['spam comments'] = 'ruslpóst umsagnir';
+$lang['Akismet API Key:'] = 'Akismet API lykill:';
+$lang['Akismet configuration'] = 'Akismet stilling';
+$lang['Akismet has caught'] = 'Akismet hefur gripið';
+$lang['More statistics on'] = 'Meiri tölfræði á';
+$lang['Signup for a personal usage key here:'] = 'Skráðu þig fyrir persónulegan notkunarlykil hér:';
+$lang['Test your configuration'] = 'Prófaðu þína uppsetningu';
+$lang['This plugin verifies if your comments are spam using the <a href="http://www.akismet.com" target="_blank">akismet online service</a>.'] = 'Þessi viðbót staðfestir hvort umsagnir þínar séu ruslpóstur með <a href="http://www.akismet.com" target="_blank">akismet netþjónustunni</a>.';
+$lang['Action when spam is detected:'] = 'Aðgerð þegar ruslpóstur finnst:';
