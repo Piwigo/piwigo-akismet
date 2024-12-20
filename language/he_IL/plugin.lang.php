ab055ab65631d03658d5a9b1dc3af3fabd4363a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:04:44 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Action when spam is detected:'] = 'פעולה כאשר זוהה דואר זבל:';
+$lang['Akismet API Key:'] = 'מפתח API של Akismet:';
+$lang['Akismet configuration'] = 'הגדרת Akismet';
+$lang['Akismet has caught'] = 'Akismet תפס';
+$lang['More statistics on'] = 'סטטיסטיקות נוספות על';
+$lang['Signup for a personal usage key here:'] = 'הרשמה למפתח שימוש אישי כאן:';
+$lang['Test your configuration'] = 'בדיקת ההגדרות שלך';
+$lang['This plugin verifies if your comments are spam using the <a href="http://www.akismet.com" target="_blank">akismet online service</a>.'] = 'התוסף הזה מאמת אם התגובות שלך הן דואר זבל באמצעות <a href="http://www.akismet.com" target="_blank">שירות האינטרנט של akismet</a>.';
+$lang['You need to define the Akismet api key'] = 'עליך להגדיר את מפתח ה- API של Akismet';
+$lang['Your Akismet API key is not valid.'] = 'מפתח ה- API שלך של Akismet אינו תקף.';
+$lang['akismet site'] = 'אתר של akismet';
+$lang['comments'] = 'תגובות';
+$lang['moderate'] = 'להפוך לרגיש';
+$lang['out of'] = 'מתוך';
+$lang['reject'] = 'לדחות';
+$lang['reset counters'] = 'לאפס את המונים';
+$lang['spam comments'] = 'תגובות זבל';
